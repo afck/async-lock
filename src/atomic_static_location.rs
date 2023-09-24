28496e0cb886082c674e1216484fa8f0fb4a2593
Mathieu Baudet <1105398+ma2bd@users.noreply.github.com>
2023-09-24 00:22:10 -0700

    store an optional location in atomic pointers

--- src/atomic_static_location.rs
+++ src/atomic_static_location.rs
@@ -1,7 +1,7 @@
 use std::panic::Location;
 use std::sync::atomic::{AtomicPtr, Ordering};
 
-/// An atomic pointer on a static Location.
+/// An atomic pointer on an optional static Location.
 #[derive(Debug)]
 pub struct AtomicStaticLocation {
     inner: AtomicPtr<Location<'static>>,
@@ -9,31 +9,42 @@ pub struct AtomicStaticLocation {
 
 impl AtomicStaticLocation {
     /// Construct an [`AtomicStaticLocation`]
-    pub fn new(initial: &'static Location<'static>) -> Self {
-        let inner = AtomicPtr::new(initial as *const _ as *mut _);
+    pub fn new(initial: Option<&'static Location<'static>>) -> Self {
+        let initial = match initial {
+            Some(r) => r as *const _ as *mut _,
+            None => std::ptr::null_mut(),
+        };
+        let inner = AtomicPtr::new(initial);
         AtomicStaticLocation { inner }
     }
 
     /// Read the Location value atomically.
-    pub fn load(&self, ordering: Ordering) -> &'static Location<'static> {
+    pub fn load(&self, ordering: Ordering) -> Option<&'static Location<'static>> {
         let ptr = self.inner.load(ordering);
-        unsafe { &*ptr }
+        unsafe { ptr.as_ref() }
     }
 
     /// Store a new Location value atomically.
-    pub fn store(&self, value: &'static Location<'static>, ordering: Ordering) {
-        self.inner.store(value as *const _ as *mut _, ordering);
+    pub fn store(&self, value: Option<&'static Location<'static>>, ordering: Ordering) {
+        let value = match value {
+            Some(r) => r as *const _ as *mut _,
+            None => std::ptr::null_mut(),
+        };
+        self.inner.store(value, ordering);
     }
 
     /// Swap the Location value atomically.
     pub fn swap(
         &self,
-        value: &'static Location<'static>,
+        value: Option<&'static Location<'static>>,
         ordering: Ordering,
-    ) -> &'static Location<'static> {
-        let new_ptr = value as *const _ as *mut _;
+    ) -> Option<&'static Location<'static>> {
+        let new_ptr = match value {
+            Some(r) => r as *const _ as *mut _,
+            None => std::ptr::null_mut(),
+        };
         let old_ptr = self.inner.swap(new_ptr, ordering);
-        unsafe { &*old_ptr }
+        unsafe { old_ptr.as_ref() }
     }
 }
 
@@ -47,14 +58,20 @@ fn test_atomic_location() {
     let location1 = get_caller_location();
     let location2 = get_caller_location();
     assert_eq!(location1.file(), file!());
-    assert_eq!(location1.line(), 43);
+    assert_eq!(location1.line(), 58);
     assert_eq!(location1.column(), 21);
 
-    let atomic_static_location = AtomicStaticLocation::new(location1);
+    let atomic_static_location = AtomicStaticLocation::new(Some(location1));
 
-    let old_value = atomic_static_location.swap(location2, Ordering::Relaxed);
-    assert_eq!(old_value, location1);
+    let old_value = atomic_static_location.swap(Some(location2), Ordering::Relaxed);
+    assert_eq!(old_value, Some(location1));
 
     let current_value = atomic_static_location.load(Ordering::Relaxed);
-    assert_eq!(current_value, location2);
+    assert_eq!(current_value, Some(location2));
+
+    let current_value = atomic_static_location.swap(None, Ordering::Relaxed);
+    assert_eq!(current_value, Some(location2));
+
+    let current_value = atomic_static_location.load(Ordering::Relaxed);
+    assert_eq!(current_value, None);
 }
